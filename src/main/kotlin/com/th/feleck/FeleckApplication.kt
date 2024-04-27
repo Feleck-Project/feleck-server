f8ec518ff08dd74668a8ef31a7d26d46ca0abb98
taehun2767 <taehun.lee@zep.us>
2024-04-27 12:12:57 +0900

    feat: initial setting (start with monolithic architecture)

---
+++ src/main/kotlin/com/th/feleck/FeleckApplication.kt
@@ -0,0 +1,11 @@
+package com.th.feleck
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeleckApplication
+
+fun main(args: Array<String>) {
+	runApplication<FeleckApplication>(*args)
+}
